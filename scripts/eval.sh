8205bb2b82186537e7cc583b070c745102e41f9b
Natnael Daba <ndaba@arizona.edu>
2025-03-19 15:59:41 -0700

    Updated eval script for Qwen-14 evaluation

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 
-MODEL=deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B
-MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,max_model_length=32768,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
+NUM_GPUS=2
+export CUDA_VISIBLE_DEVICES=2,3
+MODEL=deepseek-ai/DeepSeek-R1-Distill-Qwen-14B
+# MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,max_model_length=32768,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
+MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,tensor_parallel_size=$NUM_GPUS,max_model_length=32768,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
 OUTPUT_DIR=../data/evals/$MODEL
 
 # AIME 2024
 TASK=aime24
+export VLLM_WORKER_MULTIPROC_METHOD=spawn
 lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
     --custom-tasks ../open-r1/src/open_r1/evaluate.py \
     --use-chat-template \
